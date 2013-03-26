5e7641812a69e766c183e10498cd47d68e7d6efe
Eric Lewis <eric@webdevstudios.com>
2013-03-25 23:19:17 -0400

    gratuitous code cleanup

--- includes/functions.php
+++ includes/functions.php
@@ -506,30 +506,20 @@ function cp_validate_date( $value = NULL ) {
 	return preg_match( '`^\d{1,2}/\d{1,2}/\d{4}$`' , $value );
 }
 
-// Check user permissions
+/**
+ * Check user permissions
+ */
 function cp_check_permissions( $type = NULL ) {
-
-    //load settings user role
-    $options = get_option( 'cp_options' );
+    $options = get_option( 'cp_options' ); //load settings user role
     $cp_settings_user_role = ( isset( $options[$type] ) ) ? esc_attr( $options[$type] ) : 'manage_options';
+
     // Filter so that BP-compatibility (and other plugins) can modify
     $cp_settings_user_role = apply_filters( 'cp_settings_user_role', $cp_settings_user_role, $type );
-    if ( $cp_settings_user_role == 'all' ) :
-
+    if ( $cp_settings_user_role == 'all' )
         return true;
-
-    else :
-
-        if ( current_user_can( $cp_settings_user_role ) ) :
-
-            return true;
-
-        endif;
-
-    endif;
-
+	else if ( current_user_can( $cp_settings_user_role ) )
+        return true;
     return false;
-
 }
 
 // Clean Querystring
