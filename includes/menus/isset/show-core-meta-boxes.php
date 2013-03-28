71a397052db4f730f76cebded1116bb8e993555d
Eric Lewis <eric@webdevstudios.com>
2013-03-28 12:59:01 -0400

    Remove old code referencing dead templating functions

--- includes/menus/isset/show-core-meta-boxes.php
+++ includes/menus/isset/show-core-meta-boxes.php
@@ -10,19 +10,19 @@ global $cp_view_projects;
 global $cp_all_users_page;
 
 if ( $cp_project_page || $cp_task_list_page || $cp_task_page ) :
-	
+
 	// Task Page
 	if ( $cp_task_page ) :
-	
+
 		if ( !isset($_GET['view']) ) :
 			cp_task_comments();
 		else :
 			do_meta_boxes($this->pagehook, 'collabpress-task-edit', NULL);
 		endif;
-	
+
 	// Task List Page
 	elseif ( $cp_task_list_page ) :
-		
+
 		if ( !isset($_GET['view']) ) :
 			do_meta_boxes($this->pagehook, 'collabpress-main', NULL);
 			do_meta_boxes($this->pagehook, 'collabpress-task', NULL);
@@ -33,7 +33,7 @@ if ( $cp_project_page || $cp_task_list_page || $cp_task_page ) :
 
 	// Project Page
 	else:
-		
+
 		if ( !isset($_GET['view']) ) :
 			do_meta_boxes($this->pagehook, 'collabpress-main', NULL);
 			do_meta_boxes($this->pagehook, 'collabpress-task-list', NULL);
@@ -41,26 +41,22 @@ if ( $cp_project_page || $cp_task_list_page || $cp_task_page ) :
 		else :
 			do_meta_boxes($this->pagehook, 'collabpress-project-edit', NULL);
 		endif;
-		
+
 	endif;
 
 // User Page
 elseif ( $cp_user_page ) :
 	cp_user_page();
 
-// All Users Page
-elseif ( $cp_all_users_page ) :
-	cp_users( $limit='no' );
-
 // Calendar Page
 elseif ( $cp_calendar_page ) :
 	cp_draw_calendar();
-	
+
 // View All Projects Page
 elseif ( $cp_view_projects ) :
 	cp_view_all_projects();
 
-// Dashboard Page	
+// Dashboard Page
 else :
 	do_meta_boxes($this->pagehook, 'collabpress-main', NULL);
 	do_meta_boxes($this->pagehook, 'collabpress-project', NULL);
