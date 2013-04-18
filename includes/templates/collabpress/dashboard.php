2e4e8e5ec108bd5b0e069d5a97d92ab2493f0a05
Eric Lewis <eric@webdevstudios.com>
2013-04-18 18:24:28 -0400

    Fix check and uncheck all users functionality on Add Project modal

--- includes/templates/collabpress/dashboard.php
+++ includes/templates/collabpress/dashboard.php
@@ -47,8 +47,8 @@
 							<th scope="row"><label for="cp-project-users"><?php _e( 'Users: ', 'collabpress' ) ?></label></th>
 							<td>
 							<p>
-								<input type="button" name="CheckAll" value="<?php _e( 'Check All', 'collabpress' ); ?>" onClick="checkAll(document.new_project_form['cp_project_users[]'])" />
-								<input type="button" name="UnCheckAll" value="<?php _e( 'Uncheck All', 'collabpress' ); ?>" onClick="uncheckAll(document.new_project_form['cp_project_users[]'])" />
+								<input type="button" name="CheckAll" class="check-all-users-button" value="<?php _e( 'Check All', 'collabpress' ); ?>" />
+								<input type="button" name="UnCheckAll" class="uncheck-all-users-button" value="<?php _e( 'Uncheck All', 'collabpress' ); ?>" />
 							</p>
 							<?php
 							//check if user is subscriber
@@ -116,6 +116,20 @@
 				window.location = response.data.redirect;
 			}
 		);
+	});
+
+	// Check all users on click
+	$('.check-all-users-button').click( function() {
+		$('.cp_project_user').each( function() {
+			jQuery(this).prop('checked', 'checked');
+		});
+	});
+
+	// Uncheck all users on click
+	$('.uncheck-all-users-button').click( function() {
+		$('.cp_project_user').each( function() {
+			jQuery(this).prop('checked', '');
+		});
 	});
 })(jQuery);
 </script>
