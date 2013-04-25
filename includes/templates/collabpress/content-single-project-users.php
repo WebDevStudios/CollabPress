cf652df54c015bd027f1296648584db4bdfc6249
Eric Lewis <eric@webdevstudios.com>
2013-04-25 08:37:05 -0400

    Extra </div> in users template

--- includes/templates/collabpress/content-single-project-users.php
+++ includes/templates/collabpress/content-single-project-users.php
@@ -14,34 +14,33 @@
 		<a href="#inline_content" class="modify-users-link"><?php _e( 'Modify users', 'collabpress' ); ?></a>
 		<?php } ?>
 </div>
-	<div style='display:none'>
-		<div id='inline_content' style='padding:10px; background:#fff;'>
-		<h2><?php _e( 'Modify Users in Project', 'collabpress' ); ?></h2>
-		<input type="hidden" id="modify_project_users_nonce" value="<?php echo wp_create_nonce( 'modify-project-users' ); ?>">
-		<input type="hidden" id="cp-project-id" value="<?php echo cp_get_project_id() ?>">
-			<p>
-				<input type="button" name="CheckAll" class="check-all-users-button" value="<?php _e( 'Check All', 'collabpress' ); ?>" />
-				<input type="button" name="UnCheckAll" class="uncheck-all-users-button" value="<?php _e( 'Uncheck All', 'collabpress' ); ?>" />
-			</p>
-			<?php
-			//check if user is subscriber
-			if ( !current_user_can( 'manage_options' ) ) {
-				//if not admin, assign project to logged in user
-				echo '<input type="checkbox" name="cp_project_users[]" value="'.$current_user->ID .'" checked="checked" />&nbsp;' .$current_user->user_login .'<br />';
-			}else{
-				// @todo This fails on huge userbases
-				$wp_user_search = new WP_User_Query( array( 'fields' => 'all' ) );
-				$wp_users = $wp_user_search->get_results();
+<div style='display:none'>
+	<div id='inline_content' style='padding:10px; background:#fff;'>
+	<h2><?php _e( 'Modify Users in Project', 'collabpress' ); ?></h2>
+	<input type="hidden" id="modify_project_users_nonce" value="<?php echo wp_create_nonce( 'modify-project-users' ); ?>">
+	<input type="hidden" id="cp-project-id" value="<?php echo cp_get_project_id() ?>">
+		<p>
+			<input type="button" name="CheckAll" class="check-all-users-button" value="<?php _e( 'Check All', 'collabpress' ); ?>" />
+			<input type="button" name="UnCheckAll" class="uncheck-all-users-button" value="<?php _e( 'Uncheck All', 'collabpress' ); ?>" />
+		</p>
+		<?php
+		//check if user is subscriber
+		if ( !current_user_can( 'manage_options' ) ) {
+			//if not admin, assign project to logged in user
+			echo '<input type="checkbox" name="cp_project_users[]" value="'.$current_user->ID .'" checked="checked" />&nbsp;' .$current_user->user_login .'<br />';
+		}else{
+			// @todo This fails on huge userbases
+			$wp_user_search = new WP_User_Query( array( 'fields' => 'all' ) );
+			$wp_users = $wp_user_search->get_results();
 
-				foreach ( $wp_users as $wp_user ) {
-					echo '<input type="checkbox" name="cp_project_users[]" class="cp_project_user" value="'.$wp_user->ID .'" ' . checked( true, cp_user_is_in_project( cp_get_project_id(), $wp_user->ID ), false ) . ' />&nbsp;' .$wp_user->user_login .'<br />';
-				}
+			foreach ( $wp_users as $wp_user ) {
+				echo '<input type="checkbox" name="cp_project_users[]" class="cp_project_user" value="'.$wp_user->ID .'" ' . checked( true, cp_user_is_in_project( cp_get_project_id(), $wp_user->ID ), false ) . ' />&nbsp;' .$wp_user->user_login .'<br />';
 			}
-			?>
-			<p class="submit">
-				<input class="button-primary" type="submit" name="cp-add-project" value="<?php _e( 'Submit', 'collabpress' ); ?>"/>
-				<span class="spinner" style="float: left"></span></p>
-		</div>
+		}
+		?>
+		<p class="submit">
+			<input class="button-primary" type="submit" name="cp-add-project" value="<?php _e( 'Submit', 'collabpress' ); ?>"/>
+			<span class="spinner" style="float: left"></span></p>
 	</div>
 </div>
 <script>
