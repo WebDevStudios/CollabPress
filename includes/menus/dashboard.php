93eac144c3e4dd1f013a0ca8de42f81d5b6d4b3c
Eric Lewis <eric@webdevstudios.com>
2013-06-01 21:53:47 -0400

    better direct access detection

--- includes/menus/dashboard.php
+++ includes/menus/dashboard.php
@@ -1,10 +1,7 @@
 <?php
 
-if (!function_exists ('add_action')) {
-		header('Status: 403 Forbidden');
-		header('HTTP/1.1 403 Forbidden');
-		exit();
-}
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
 
 // CollabPress Pages
 $cp_dashboard_page = false;
