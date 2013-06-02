d0c2c008f75f1d76c358a53165c7669d62173042
Eric Lewis <eric@webdevstudios.com>
2013-06-01 21:34:58 -0400

    move cp_recent_activity to functions.php

--- includes/admin_init.php
+++ includes/admin_init.php
@@ -53,6 +53,8 @@ function cp_admin_init() {
 
 }
 
+add_action( 'admin_init', 'cp_dismiss_admin_notice' );
+
 /**
  * Dismiss a CP warning admin notice
  *
@@ -82,104 +84,4 @@ function cp_dismiss_admin_notice() {
 
 	$redirect = remove_query_arg( array( '_wpnonce', 'cp_dismiss' ), wp_get_referer() );
 	wp_safe_redirect( $redirect );
-}
-add_action( 'admin_init', 'cp_dismiss_admin_notice' );
-
-
-// Show Recent Activity
-function cp_recent_activity($data = NULL) {
-
-	// Get Current User
-	global $current_user;
-	get_currentuserinfo();
-
-	// Get Activities
-	$paged = (isset($_GET['paged'])) ? esc_html($_GET['paged']) : 1;
-
-	// Load plugin options
-	$cp_options = get_option( 'cp_options' );
-
-	// Check number of recent items to display
-	$cp_num_recent = ( isset( $cp_options['num_recent_activity'] ) ) ? absint( $cp_options['num_recent_activity'] ) : 4;
-
-	$activities_args = array( 'post_type' => 'cp-meta-data', 'showposts' => $cp_num_recent, 'paged' => $paged );
-	$activities_query = new WP_Query( $activities_args );
-
-	echo '<div class="cp-activity-list">';
-
-	// WP_Query();
-	if ( $activities_query->have_posts() ) :
-	$activityCount = 1;
-	while( $activities_query->have_posts() ) : $activities_query->the_post();
-		    global $post;
-
-		    if ( ($activityCount % 2) == 0 ) {
-			    $row = " even";
-		    } else {
-			    $row = " odd";
-		    }
-
-		    // Avatar
-		    $activityUser = get_post_meta($post->ID, '_cp-activity-author', true);
-		    $activityUser = get_userdata($activityUser);
-		    $activityAction = get_post_meta($post->ID, '_cp-activity-action', true);
-		    $activityType = get_post_meta($post->ID, '_cp-activity-type', true);
-		    $activityID = get_post_meta($post->ID, '_cp-activity-ID', true);
-
-		    if ( $activityUser ) :
-		    ?>
-
-		    <div class="cp-activity-row <?php echo $row ?>">
-			    <a class="cp-activity-author" title="<?php $activityUser->display_name ?>" href="<?php echo COLLABPRESS_DASHBOARD; ?>&user=<?php echo $activityUser->ID ?>"><?php echo get_avatar($activityUser->ID, 32) ?></a>
-			    <div class="cp-activity-wrap">
-			    <p class="cp-activity-description"><?php echo $activityUser->display_name . ' ' . $activityAction . ' ' . __('a', 'collabpress') . ' '. $activityType ?>: <a href="<?php echo cp_get_url( $activityID, $activityType ); ?>"><?php echo get_the_title( $activityID ); ?></a></p>
-			    </div>
-		    </div>
-
-		    <?php
-		    endif;
-		    $activityCount++;
-	endwhile;
-	wp_reset_query();
-	else :
-		echo '<p>'.__( 'No Activities...', 'collabpress' ).'</p>';
-	endif;
-
-	// Pagination
-	if ( $activities_query->max_num_pages > 1 ) {
-		echo '<p class="cp_pagination">';
-	    for ( $i = 1; $i <= $activities_query->max_num_pages; $i++ ) {
-	        echo '<a href="'.COLLABPRESS_DASHBOARD.'&paged='.$i.'" '.(($paged == $i) ? 'class="active"' : '' ).'>'.$i.'</a> ';
-	    }
-	    echo '</p>';
-	} ?>
-
-	<style type="text/css">
-		.cp-activity-list {
-		    position: relative;
-		}
-		.cp-activity-row {
-		    margin: 0;
-		    overflow: hidden;
-		    padding: 2px 10px;
-		}
-		.cp-activity-list .even {
-		    background-color: #FFFFE0;
-		}
-		.cp-activity-list .cp-activity-author {
-		    float: left;
-		    margin: 5px 0;
-		}
-		.cp-activity-list .cp-activity-wrap {
-		    margin: 6px 0;
-		    overflow: hidden;
-		    word-wrap: break-word;
-		}
-		.cp-activity-list p {
-		    font-size: 11px;
-		    margin: 6px 6px 8px;
-		}
-	</style>
-
-	<?php echo '</div>';
 }
