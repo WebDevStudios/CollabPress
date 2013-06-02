4b07fa98b45ec1248652d562136127d5a812ab05
Eric Lewis <eric@webdevstudios.com>
2013-06-01 21:53:00 -0400

    remove unused functions

--- includes/menus.php
+++ includes/menus.php
@@ -8,16 +8,6 @@ function cp_settings_page() {
 	require_once( 'menus/settings.php' );
 }
 
-// Settings
-function cp_help_page() {
-	require_once( 'menus/help.php' );
-}
-
-// Debug
-function cp_debug_page() {
-	require_once( 'menus/debug.php' );
-}
-
 // Footer
 function cp_footer() {
     require_once( 'footer.php' );
